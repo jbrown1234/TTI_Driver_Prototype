7b2bed43cc27227cb893ffc33c9a9e475c12a492
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2022-03-04 20:35:44 -0500

    Update 006_Battery_Discharge.py
    1. Corrected rear selection check
    2. Started adding in error return information
    3. Layered in source and measure setup calls

--- 006_Battery_Discharge.py
+++ 006_Battery_Discharge.py
@@ -49,13 +49,46 @@ def configure_system(do_beeps):
     if "F" in selection.upper():
         smu.terminals = smuconst.TERMINALS_FRONT
         test_parameters['terminals'] = smuconst.TERMINALS_FRONT
-    elif "T" in selection.upper():
+    elif "R" in selection.upper():
         smu.terminals = smuconst.TERMINALS_REAR
         test_parameters['terminals'] = smuconst.TERMINALS_REAR
+    else:
+        return -1010, "configure_system aborted by user"
 
     # Configure the source settings
     smu.source.func = smuconst.FUNC_DC_CURRENT
     smu.source.offmode = smuconst.OFFMODE_HIGHZ
     smu.source.readback = smuconst.OFF
 
-    return
+    smu.source.level = 0.0          # Amps; zero is default value
+    smu.source.range = 0.001        # Amps; automatically disables source autorange.  Was 0.1;
+    smu.source.delay = 0.0          # Seconds; automatically disables source autodelay
+
+    # Configure measure settings
+    smu.measure.function = smuconst.FUNC_DC_VOLTAGE
+    smu.measure.sense = smuconst.SENSE_4WIRE
+
+    temp_str = smu.localnode.model()
+    if "2450" in temp_str:
+        smu.measure.range = 200.0           # volts
+        smu.source.vlimit.level = 210.0     # volts
+    elif "2460" in temp_str:
+        smu.measure.range = 100.0           # volts
+        smu.source.vlimit.level = 105.0     # volts
+    elif "2461" in temp_str:
+        smu.measure.range = 100.0           # volts
+        smu.source.vlimit.level = 105       # volts
+    elif "2470" in temp_str:
+        smu.measure.range = 200.0           # volts
+        smu.source.vlimit.level = 210       # volts
+    else:
+        return -1011, "Unexpected SMU model detected; configure_system aborted"
+
+    smu.measure.autorange = smuconst.ON
+    smu.measure.nplc = 5.0
+
+    return 0, ""
+
+
+smu.initialize("USB0::0x05E6::0x2460::04312353::INSTR")
+configure_system(True)
