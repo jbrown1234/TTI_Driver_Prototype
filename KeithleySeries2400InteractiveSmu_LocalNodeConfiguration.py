5a2435ed2409941f08437d02af57ccad2388a3a5
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2022-03-04 20:37:48 -0500

    Create KeithleySeries2400InteractiveSmu_LocalNodeConfiguration.py
    New addition!

---
+++ KeithleySeries2400InteractiveSmu_LocalNodeConfiguration.py
@@ -0,0 +1,25 @@
+import KeithleySeries2400InteractiveSmu_Constants as _SmuConst
+
+
+class LocalNodeConfiguration:
+    def __init__(self):
+        self._mycomms = None
+        # self.input = self.Input()
+
+    def update_comms(self):
+        """
+        This function is used to ensure lower level consumer classes tied to the driver are updated to promote \
+        instrument communications.
+
+        :return:
+        """
+        # self.input._mycomms = self._mycomms
+
+    def model(self):
+        """
+        This attribute stores the model number.
+
+        :return: The model number of the instrument
+        """
+        return self._mycomms.query("print(localnode.model)").rstrip()
+
