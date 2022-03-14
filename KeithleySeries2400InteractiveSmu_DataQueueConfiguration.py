6e74e430738bddd4d3d4fd01915e6f3afeb1ba80
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2022-03-13 22:31:04 -0400

    Create KeithleySeries2400InteractiveSmu_DataQueueConfiguration.py
    New addition

---
+++ KeithleySeries2400InteractiveSmu_DataQueueConfiguration.py
@@ -0,0 +1,44 @@
+#  Copyright 2022 Joshua Brown
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import KeithleySeries2400InteractiveSmu_Constants as _smuconst
+
+
+class DataQueueConfiguration:
+    """
+    Placeholder docstring description.
+    """
+    def __init__(self):
+        self._mycomms = None
+        # self.input = self.Input()
+
+    def update_comms(self):
+        """
+        This function is used to ensure lower level consumer classes tied to\
+            the driver are updated to promote instrument communications.
+
+        :return:
+        """
+        # self.input._mycomms = self._mycomms
+        print(0)
+
+    def add(self, value, timeout=None):
+        """
+        This function adds an entry to the data queue.
+
+        :param value: (int)
+        :param timeout: (float)
+        :return
+        """
+        print(0)
