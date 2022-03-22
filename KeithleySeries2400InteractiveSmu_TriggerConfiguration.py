fc9b0c02a82af13480338fefead395a677d5df4c
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2022-03-21 21:45:30 -0400

    Update KeithleySeries2400InteractiveSmu_TriggerConfiguration.py
    Formatting updates

--- KeithleySeries2400InteractiveSmu_TriggerConfiguration.py
+++ KeithleySeries2400InteractiveSmu_TriggerConfiguration.py
@@ -25,8 +25,8 @@ class TriggerConfiguration:
 
     def update_comms(self):
         """
-        This function is used to ensure lower level consumer classes tied to the driver are updated to promote \
-        instrument communications.
+        This function is used to ensure lower level consumer classes tied to\
+            the driver are updated to promote instrument communications.
 
         :return:
         """
@@ -51,46 +51,61 @@ class TriggerConfiguration:
 
         def load_duration_loop(self, duration, delay=None, buffer_name=None):
             """
-            This function loads a trigger-model template configuration that makes continuous measurements for a
-            specified amount of time.
+            This function loads a trigger-model template configuration that\
+                makes continuous measurements for a specified amount of time.
 
-            :param duration: The amount of time for which to make measurements (167 ns to 100 ks)
-            :param delay: The delay time before each measurement (167 ns to 10 ks); default is 0 for no delay
-            :param buffer_name: The name of the reading buffer, which may be a default buffer (defbuffer1 or defbuffer2) or a user-defined buffer; defaults to defbuffer1
+            :param duration: The amount of time for which to make measurements\
+                (167 ns to 100 ks)
+            :param delay: The delay time before each measurement (167 ns to 10\
+                ks); default is 0 for no delay
+            :param buffer_name: The name of the reading buffer, which may be a\
+                default buffer (defbuffer1 or defbuffer2) or a user-defined\
+                    buffer; defaults to defbuffer1
             :return: None
             """
             if buffer_name is not None:
-                self._mycomms.write(f"trigger.model.load(\"DurationLoop\", {duration}, {delay}, {buffer_name})")
+                self._mycomms.write(f"trigger.model.load(\"DurationLoop\",\
+                    {duration}, {delay}, {buffer_name})")
             elif delay is not None:
-                self._mycomms.write(f"trigger.model.load(\"DurationLoop\", {duration}, {delay})")
+                self._mycomms.write(f"trigger.model.load(\"DurationLoop\",\
+                    {duration}, {delay})")
             else:
-                self._mycomms.write(f"trigger.model.load(\"DurationLoop\", {duration})")
+                self._mycomms.write(f"trigger.model.load(\"DurationLoop\",\
+                    {duration})")
 
             print(0)
 
         def load_simple_loop(self, count, delay=None, buffer_name=None):
             """
-            This function loads a trigger-model template configuration that makes a specific number of measurements.
+            This function loads a trigger-model template configuration that\
+                makes a specific number of measurements.
 
             :param count: The number of measurements the instrument will make.
-            :param delay: The delay time before each measurement (167 ns to 10 ks); default is 0 for no delay.
-            :param buffer_name: Indicates the reading buffer to use; the default buffers (defbuffer1 or
-            defbuffer2) or the name of a user-defined buffer; if no buffer is specified, defbuffer1 is used.
+            :param delay: The delay time before each measurement (167 ns to 10\
+                ks); default is 0 for no delay.
+            :param buffer_name: Indicates the reading buffer to use; the\
+                default buffers (defbuffer1 or
+            defbuffer2) or the name of a user-defined buffer; if no buffer is\
+                specified, defbuffer1 is used.
             :return: None
             """
             if buffer_name is not None:
-                self._mycomms.write(f"trigger.model.load(\"SimpleLoop\", {count}, {delay}, {buffer_name})")
+                self._mycomms.write(f"trigger.model.load(\"SimpleLoop\",\
+                    {count}, {delay}, {buffer_name})")
             elif delay is not None:
-                self._mycomms.write(f"trigger.model.load(\"SimpleLoop\", {count}, {delay})")
+                self._mycomms.write(f"trigger.model.load(\"SimpleLoop\",\
+                    {count}, {delay})")
             else:
-                self._mycomms.write(f"trigger.model.load(\"SimpleLoop\", {count})")
+                self._mycomms.write(f"trigger.model.load(\"SimpleLoop\",\
+                    {count})")
 
         @property
         def state(self):
             """
             This function returns the present state of the trigger model.
 
-            :return: Will return one of the following: ABORTED, ABORTING, BUILDING, EMPTY, FAILED, IDLE, RUNNING
+            :return: Will return one of the following: ABORTED, ABORTING,\
+                BUILDING, EMPTY, FAILED, IDLE, RUNNING
             """
             self._mycomms.write("stat, stat, n = trigger.model.state()")
             status = self._mycomms.query("print(stat)")
